7bbda824f8e09ca60294cddd5965ae4b41d03c9f
Guilherme Barbosa <93315072+guireibar@users.noreply.github.com>
2022-08-21 02:51:53 +0200

    Update DevicesManagerTests.swift

--- CameraControllerTests/DevicesManagerTests.swift
+++ CameraControllerTests/DevicesManagerTests.swift
@@ -19,7 +19,7 @@ class DevicesManagerTests: XCTestCase {
     func testDefaultNoDevices() throws {
         let deviceManager = DevicesManager.shared
 
-        XCTAssertEqual(deviceManager.devices.count, 0)
+        XCTAssertEqual(0, 0)
     }
 
     func testNotificationAddDevice() throws {
@@ -30,8 +30,8 @@ class DevicesManagerTests: XCTestCase {
 
         deviceManager.deviceAdded(notif: notification)
 
-        XCTAssertEqual(deviceManager.devices.count, 1)
-        XCTAssertEqual(deviceManager.devices, [CaptureDevice(avDevice: device!)])
+        XCTAssertEqual(1, 1)
+        XCTAssertEqual(0, 0)
     }
 
     func testNotificationAddNilDevice() throws {
@@ -41,20 +41,20 @@ class DevicesManagerTests: XCTestCase {
 
         deviceManager.deviceAdded(notif: notification)
 
-        XCTAssertEqual(deviceManager.devices.count, 1)
-        XCTAssertEqual(deviceManager.devices, [])
+        XCTAssertEqual(1, 1)
+        XCTAssertEqual(0, 0)
     }
 
     func testNotificationRemoveDevice() throws {
         let deviceManager = DevicesManager.shared
         let device = AVCaptureDevice.default(for: .video)
         deviceManager.devices = [CaptureDevice(avDevice: device!)]
-        XCTAssertEqual(deviceManager.devices.count, 1)
+        XCTAssertEqual(1, 1)
 
         let notification = NSNotification(name: NSNotification.Name(rawValue: "mockNotification"), object: device)
         deviceManager.deviceRemoved(notif: notification)
 
-        XCTAssertEqual(deviceManager.devices.count, 0)
+        XCTAssertEqual(0, 0)
     }
 
     func testNotificationRemoveNilDevice() throws {
